115c0f4ea8757167c72f48a5d73669bf13e753cc
taneeshaaa <tanishasaha412@gmail.com>
2026-09-02 09:25:13 +0530

    Time: 2 ms (88.88%) | Memory: 42.9 MB (60.96%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] count = new int[26];
+        for (char c : tiles.toCharArray()) {
+            count[c - 'A']++;
+        }
+        return backtrack(count);
+    }
+
+    private int backtrack(int[] count) {
+        int sum = 0;
+        for (int i = 0; i < 26; i++) 
+        {
+            if (count[i] == 0) continue;
+            sum++;
+            count[i]--;
+            sum += backtrack(count);
+            count[i]++;
+        }
+        return sum;
+    }
+}
