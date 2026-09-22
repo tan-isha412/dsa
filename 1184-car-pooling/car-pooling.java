78e83ec60f08f1d08c590ee98e8df90c28f08394
taneeshaaa <tanishasaha412@gmail.com>
2026-09-22 07:57:20 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (24.91%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int n=trips.length;
-        if(n<1) return true;
-        int minm=Integer.MAX_VALUE,maxm=0;
+        int maxm=-1,minm=Integer.MAX_VALUE;
         for(int[] t:trips)
         {
             minm=Math.min(minm,t[1]);
             maxm=Math.max(maxm,t[2]);
         }
-        int[] cap=new int[maxm-minm+1];
+        int[] c=new int[maxm-minm+1];
         for(int[] t:trips)
         {
-            cap[t[1]-minm]+=t[0];
-            cap[t[2]-minm]-=t[0];
+            c[t[1]-minm]+=t[0];
+            c[t[2]-minm]-=t[0];
         }
         int sum=0;
-        for(int c:cap)
+        for(int cap:c)
         {
-            sum+=c;
-            if(sum>capacity) return false;
+            sum+=cap;
+            if(sum>capacity)
+                return false;
         }
         return true;
     }
